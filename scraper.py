a0defb7517922833599d06f8ecbd20e8f583031f
Mike Boring <shekinah999@msn.com>
2020-07-23 16:37:48 -0600

    Setup Files

---
+++ scraper.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Simple Web Scraper.
+
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+__author__ = "Mike Boring"
+
+"""
+Application to scrape a single web page, extracting any URLs, email addresses, and phone numbers it contains.
+...
+
+Suggested milestones for incremental development:
+ - Setup parser for command line parsing
+ - Save specified webpage html to a text file
+ - Search for email, phone and url addresses in file using findall or RE
+ - Save to dictionary of lists with keys of url, phone, email
+ - Print to console a separated list of each
+"""
+
