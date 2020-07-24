57c51228025656005ec5c5cf6708d5aa1ed8a10d
Mike Boring <shekinah999@msn.com>
2020-07-23 18:39:53 -0600

    Completed Application

--- scraper.py
+++ scraper.py
@@ -10,14 +10,84 @@
 __author__ = "Mike Boring"
 
 """
-Application to scrape a single web page, extracting any URLs, email addresses, and phone numbers it contains.
+Application to scrape a single web page, extracting any URLs, email addresses,
+and phone numbers it contains.
 ...
 
 Suggested milestones for incremental development:
- - Setup parser for command line parsing
- - Save specified webpage html to a text file
- - Search for email, phone and url addresses in file using findall or RE
- - Save to dictionary of lists with keys of url, phone, email
- - Print to console a separated list of each
+ DONE- Setup parser for command line parsing
+ DONE- Save specified webpage html to a text file
+ DONE- Search for email, phone and url addresses in file using findall or RE
+ DONE- Save to dictionary of lists with keys of url, phone, email
+ DONE- Print to console a separated list of each
 """
 
+import re
+import sys
+import urllib.request
+import argparse
+import os
+
+
+def create_parser():
+    """Creates an argument parser object."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument('selected_url', help='url to extract data from')
+    return parser
+
+
+def find_data(selected_url):
+    """
+    Search thru specified url for urls, phone
+    numbers and email addresses and print to console.
+    """
+    found_data = {}
+    if os.path.isfile('url_to_search.txt'):
+        os.remove('url_to_search.txt')
+    urllib.request.urlretrieve(selected_url, 'url_to_search.txt')
+    with open('url_to_search.txt') as f:
+        text = f.read()
+        final_url_list = []
+        url_format = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+        email_format = r'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)'
+        phone_format = r'(\([1-9][0-9][0-9]\)[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]|[1-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9])'
+        found_data['url'] = re.findall(url_format, text)
+        for url in found_data['url']:
+            split_url = url.split("'")
+            final_url_list.append(split_url[0])
+        found_data['url'] = final_url_list
+        found_data['email'] = re.findall(email_format, text)
+        found_data['phone'] = re.findall(phone_format, text)
+    print('URLS:\n')
+    if found_data['url']:
+        for url in set(found_data['url']):
+            print(url)
+    else:
+        print('NONE')
+    print('\nEmails:\n')
+    if found_data['email']:
+        for email in set(found_data['email']):
+            print(email)
+    else:
+        print('NONE')
+    print('\nPhone Numbers:\n')
+    if found_data['phone']:
+        for phone in set(found_data['phone']):
+            print(phone)
+    else:
+        print('NONE')
+
+
+def main(args):
+    """Parses args."""
+    parser = create_parser()
+    if not args:
+        parser.print_usage()
+        sys.exit(1)
+    parsed_args = parser.parse_args(args)
+    selected_url = parsed_args.selected_url
+    find_data(selected_url)
+
+
+if __name__ == '__main__':
+    main(sys.argv[1:])
